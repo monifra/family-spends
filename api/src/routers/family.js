df47dbf68e2120c74242221c8bc126275dde8e56
Monika Frankowska <monia.frankowska@gmail.com>
2020-09-26 20:23:25 +0200

    add patch routes and delete routes to family.js and user.js routers

--- api/src/routers/family.js
+++ api/src/routers/family.js
@@ -17,17 +17,62 @@ function asyncHandler(cb){
 
 //USERS ENDPOINTS
 
-//get families api/families, show all families, status 200
+//get families api/families, show all families, status 200 OK
 router.get('/families', asyncHandler(async(req,res)=>{
     const families = await Family.find({});
     res.status(200).send(families);
 }));
 
+//get single family by its id, api/families/:id, status 200 OK
+router.get('/families/:id', asyncHandler(async(req,res)=>{
+    const _id = req.params.id;
+    const family = await Family.findById(_id);
+    if(!family){
+        res.status(404).send({error: 'Family doesn\'t exist'}) ;//show error message when family doesn't exist
+    } else {
+        res.status(200).send(family);
+    }
+}));
+
 //post family api/families, post new family, status 201 Created
-router.post('families', asyncHandler(async(req,res)=>{
+router.post('/families', asyncHandler(async(req,res)=>{
     const family = new Family(req.body);
     await family.save();
     res.status(201).send(family);
+}));
+
+//patch one family api/families/:id, status 200 OK
+router.patch('/families/:id', asyncHandler(async(req,res)=>{
+    const _id = req.params.id;
+    const body = req.body;
+
+    //verify existence in a model
+    const updates = Object.keys(body);
+    const allowUpdates = ['familyName', 'familyMembers', 'savings'];
+    const isValid = updates.every((update)=> allowUpdates.includes(update));
+
+    if(!isValid){
+        return res.status(404).send({error: 'Invalid Updates!'});
+    }
+
+    const family =  await Family.findByIdAndUpdate(_id, body, {new: true, runValidators: true});
+    if(!family){
+        res.status(404).send({error: 'Family doesn\'t exist'});
+    } else {
+        res.status(200).send(family);
+    }
+
+}));
+
+//delete one family api/families/:id, status 200 OK
+router.delete('/families/:id', asyncHandler(async(req, res)=>{
+    const _id = req.params.id;
+    const family = await Family.findByIdAndDelete(_id);
+    if(!family){
+        res.status(404).send({error: 'Family doesn\'t exist'});
+    }else{
+        res.send({message: family.familyName + ' family was deleted.'});
+    }
 }));
 
 module.exports = router;
--- api/src/routers/user.js
+++ api/src/routers/user.js
@@ -23,6 +23,45 @@ router.get('/users', asyncHandler(async(req,res)=>{
     res.status(200).send(users);
 }));
 
-//post
+//post user api/users, post new user, status 201 Created
+router.post('/users', asyncHandler(async(req,res)=>{
+    const user = new User(req.body);
+    await user.save();
+    res.status(201).send(user);
+}));
+
+//patch one user api/users/:id, status 200 OK
+router.patch('/users/:id', asyncHandler(async(req,res)=>{
+    const _id = req.params.id;
+    const body = req.body;
+
+    //verify existence in a model
+    const updates = Object.keys(body);
+    const allowUpdates = ['name', 'familyName', 'email', 'password'];
+    const isValid = updates.every((update)=> allowUpdates.includes(update));
+
+    if(!isValid){
+        return res.status(404).send({error: 'Invalid Updates!'});
+    }
+
+    const user =  await User.findByIdAndUpdate(_id, body, {new: true, runValidators: true});
+    if(!user){
+        res.status(404).send({error: 'User doesn\'t exist'});
+    } else {
+        res.status(200).send(user);
+    }
+
+}));
+//TO TEST
+//delete one user api/users/:id, status 200 OK
+router.delete('/users/:id', asyncHandler(async(req, res)=>{
+    const _id = req.params.id;
+    const user = await User.findByIdAndDelete(_id);
+    if(!user){
+        res.status(404).send({error: 'User doesn\'t exist'});
+    }else{
+        res.send({message: user.name + ' ' + user.familyName + ' was deleted.'});
+    }
+}));
 
 module.exports = router;
