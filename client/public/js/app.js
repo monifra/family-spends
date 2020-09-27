4ffb7f40c5621044983a29e00a0d3da0695d7d65
Monika Frankowska <monia.frankowska@gmail.com>
2020-09-27 19:03:26 +0200

    add postman-request to packages, add requests to family api endpoints

---
+++ client/public/js/app.js
@@ -0,0 +1,8 @@
+// const url = 'http://localhost:3000/api/familis';
+//
+// fetch(url).then((res)=>{
+//     res.json().then((data)=>{
+//         console.log(data);
+//     })
+// });
+
--- client/src/app.js
+++ client/src/app.js
@@ -1,1 +1,40 @@
 //file for routing
+const request = require('postman-request');
+
+const familyURL = 'http://localhost:3000/api/families';
+const familySingleURL = 'http://localhost:3000/api/families/5f6f328a33ced1c816a037c8';
+
+
+request({url: familyURL}, (err,res)=>{
+    if(err){
+        console.log('Unable to connect to family spends api');
+    } else {
+        const body = JSON.parse(res.body);
+        const bodyError = body.error;
+
+        if(bodyError === 'Page Not Found'){
+            console.log(bodyError);
+            console.log('statusCode:', res && res.statusCode);
+        }else{
+            console.log(res.body);
+            console.log('statusCode:', res && res.statusCode);
+        }
+    }
+});
+
+request({url: familySingleURL}, (err,res)=> {
+    if (err) {
+        console.log('Unable to connect to family spends api');
+    } else {
+        const body = JSON.parse(res.body);
+        const bodyError = body.error;
+
+        if (bodyError === 'Family doesn\'t exist') {
+            console.log(bodyError);
+            console.log('statusCode:', res && res.statusCode);
+        } else {
+            console.log(res.body);
+            console.log('statusCode:', res && res.statusCode);
+        }
+    }
+});
